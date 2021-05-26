0fd69b699b67010e93c70cef0e94126951ea6a1e
DENISE EVELIN <deniseevelynk3@hotmail.com>
2021-05-26 20:04:08 -0300

    Atualizando rotas

--- routes/products-routes.js
+++ routes/products-routes.js
@@ -2,13 +2,29 @@ const controller = require("../controllers/products-controller");
 
 module.exports = (app) => {
 
-  app.get("/products/:id", controller.getProductById);
-
-  app.get("/products", controller.getAllProducts);
-
   app.post("/products/register", controller.registerProducts);
+
+  app.get("/products/:id", controller.getProductById);
 
   app.patch("/products/:id", controller.updateProduct);
 
   app.delete("/products/:id", controller.removeProduct);
+
+  app.get("/products", controller.getAllProducts);
+
+//   router.prefix('/products', authMiddleware, async function (user) {
+    
+//     user.route('/register')
+//     .post(controller.registerProducts);
+    
+//     user.route('/:id')
+//     .get(controller.getProductById)
+//     .patch(controller.updateProduct)
+//     .delete(controller.removeProduct);
+    
+//     user.route('/')
+//     .get(controller.getAllProducts);
+
+// });
 };
+
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,11 +2,13 @@ const controller = require("../controllers/users-controller");
 const middleware = require("../middleware/auth");
 
 module.exports = (app) => {
-  app.get("/users", middleware, controller.getAllUsers);
-
-  app.get("/users/:id", controller.getUserById);
-
+  
   app.post("/users/register", controller.registerUser);
 
   app.post("/users/auth", controller.userAuthentication);
+  
+  app.get("/users/:id", controller.getUserById);
+
+  app.get("/users", middleware, controller.getAllUsers);
+
 };
