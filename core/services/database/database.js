c30e0a8fb1f38f7eb54e399bfd7121b4273bf436
DENISE EVELIN <deniseevelynk3@hotmail.com>
2021-06-15 09:28:44 -0300

    .

--- core/services/database/database.js
+++ core/services/database/database.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
-const mongo =  process.env.MONGO_URL
+const mongo = process.env.MONGO_URL;
 
 mongoose.set("useCreateIndex", true);
 
-mongoose.connect(
-  mongo,
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+mongoose.connect(mongo, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
